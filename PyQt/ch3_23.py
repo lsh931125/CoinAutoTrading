53e4b10f79d93a222fed594612a65dd51cc7b00e
Your Name <you@example.com>
2021-06-04 19:56:52 +0900

    main

---
+++ PyQt/ch3_23.py
@@ -0,0 +1,11 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+import pykorbit
+
+form_class = uic.loadUiType("untitled.ui")[0]
+
+class MyWindow(QMainWindow, form_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
